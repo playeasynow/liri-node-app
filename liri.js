4f72356322cc24916a1787c5a91a70ff2d49429b
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-10-17 18:13:11 -0500

    added dotenv, init, spotify-api

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const keys = require("./keys.js");
+
+const Spotify = require("node-spotify-api");
+
+// var spotify = new Spotify(keys.spotify);
+
+console.log(Spotify);
