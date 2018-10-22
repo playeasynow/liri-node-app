7ff9bf86691b81a1e93c00d56a3337aaf07c49f4
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2018-10-22 17:33:20 -0500

    cleaned up doWhatItSays function

--- liri.js
+++ liri.js
@@ -1,109 +1,169 @@
+// our require files
 require("dotenv").config();
 const fs = require("fs");
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 const request = require("request");
 const inquirer = require("inquirer")
+const spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
-
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'command',
-      message: 'What would like liri to do?',
-      choices: ['Find a Concert', 'Find a song on Spotify', 'Find a Movie', 'Do as I say']
-    }
-  ])
+// starts initial question for various commands
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'command',
+    message: 'What would like liri to do?',
+    choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+  }
+])
   .then(answers => {
     chosenAPI(answers.command);
   });
 
+// our main switch function
 function chosenAPI(value) {
-
   switch (value) {
-    case "Find a song on Spotify":
-      inquirer.prompt([{
-        type: 'input',
-        name: 'song',
-        message: "Name a song:"
-      }, {
-        type: 'input',
-        name: 'artist',
-        message: "Name the artist:"
-      }
-      ]).then(answers => {
-        // console.log(answers);
-        spotify.search({ type: 'track', query: (answers.song + ", " + answers.artist) }, function (err, data) {
-          if (err) {
-            return console.log('Error occurred: ' + err);
-          }
-          console.log("Here is '" + data.tracks.items[0].name + "' from the album, " + data.tracks.items[0].album.name + ", by " + data.tracks.items[0].album.artists[0].name);
-          console.log("Spotify Link: " + data.tracks.items[0].preview_url);
-        });
-      });
+    case "spotify-this-song":
+      searchSpotify();
       break;
-    case "Find a Concert":
-      inquirer.prompt([{
-        type: 'input',
-        name: 'artist',
-        message: "Name an artist:"
-      }
-      ]).then(answers => {
-        // console.log(answers);
-        console.log(answers);
-        var queryURL = "https://rest.bandsintown.com/artists/" + answers.artist + "/events?app_id=codingbootcamp";
-
-        request(queryURL, function (err, response, body) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(body[0]);
-          }
-        })
-      });
+    case "concert-this":
+      findConcert();
       break;
-    case "Find a Movie":
-      // code block
-      inquirer.prompt([{
-        type: 'input',
-        name: 'movie',
-        message: "Name a movie:"
-      }
-      ]).then(answers => {
-        // console.log(answers);
-        console.log(answers);
-        if (answers.movie === undefined) {
-          answers.movie = "mr nobody";
-        }
-        // HTTP GET request
-        request("http://www.omdbapi.com/?t=" + answers.movie + "&y=&plot=short&r=json", function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-            console.log("* Title of the movie:         " + JSON.parse(body).Title);
-            console.log("* Year the movie came out:    " + JSON.parse(body).Year);
-            console.log("* IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
-            console.log("* Country produced:           " + JSON.parse(body).Country);
-            console.log("* Language of the movie:      " + JSON.parse(body).Language);
-            console.log("* Plot of the movie:          " + JSON.parse(body).Plot);
-            console.log("* Actors in the movie:        " + JSON.parse(body).Actors);
-
-            // For loop parses through Ratings object to see if there is a RT rating
-            // 	--> and if there is, it will print it
-            for (var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-              if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-                console.log("* Rotten Tomatoes Rating:     " + JSON.parse(body).Ratings[i].Value);
-                if (JSON.parse(body).Ratings[i].Website !== undefined) {
-                  console.log("* Rotten Tomatoes URL:        " + JSON.parse(body).Ratings[i].Website);
-                }
-              }
-            }
-          }
-        })
-      });
+    case "movie-this":
+      getMovieInfo();
       break;
     case "do-what-it-says":
-      // code block
+      doWhatItSays();
+      logData();
       break;
+  }
+}
+
+// ALL LOGIC FUNCTIONS
+
+// finds song + preview link via spotify api
+function searchSpotify() {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'song',
+    message: "Name a song:"
+  }, {
+    type: 'input',
+    name: 'artist',
+    message: "Name the artist:"
+  }
+  ]).then(answers => {
+    spotify.search({ type: 'track', query: (answers.song + ", " + answers.artist) }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      console.log("Here is '" + data.tracks.items[0].name + "' from the album, " + data.tracks.items[0].album.name + ", by " + data.tracks.items[0].album.artists[0].name);
+      console.log("Spotify Link: " + data.tracks.items[0].preview_url);
+    });
+  });
+}
+
+// read from random.txt file
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+
+    var songdataArray = data.split(",");
+
+    function chosenAPI1() {
+      switch (songdataArray[0]) {
+        case "spotify-this-song":
+          spotify.search({ type: 'track', query: (songdataArray[1]) }, function (err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+            console.log("Here is '" + data.tracks.items[0].name + "' from the album, " + data.tracks.items[0].album.name + ", by " + data.tracks.items[0].album.artists[0].name);
+            console.log("Spotify Link: " + data.tracks.items[0].preview_url);
+          });
+          break;
+        case "concert-this":
+          findConcert();
+          break;
+        case "movie-this":
+          getMovieInfo();
+          break;
+        case "do-what-it-says":
+          doWhatItSays();
+      }
+    }
+
+    chosenAPI1();
+  });
+}
+
+// log the data to the terminal/bash window, output the data to a .txt file called log.txt.
+function logData(logResults) {
+  fs.appendFile("log.txt", logResults + "\r\n", function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log("Content Added!");
+    }
+  });
+}
+
+// finds movie using OMDB api
+function getMovieInfo() {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'movie',
+    message: "Name a movie:"
+  }
+  ]).then(answers => {
+    if (!answers.movie) {
+      answers.movie = "Mr Nobody";
+      console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!")
+    }
+
+    request("http://www.omdbapi.com/?t=" + answers.movie + "&apikey=trilogy", function (error, response, body) {
+
+      if (!error && response.statusCode === 200) {
+        var movieInfo = JSON.parse(body);
+
+        var movieResult =
+          //Line break
+          "=======================================================================================================" + "\r\n" +
+          //Output the liri command plus answers.movie
+          "movie-this ---> " + answers.movie + "\r\n" +
+          //Line break
+          "=======================================================================================================" + "\r\n" +
+          // title of the movie
+          "Title: " + movieInfo.Title + "\r\n" +
+          // year the movie came out
+          "Year movie was released: " + movieInfo.Year + "\r\n" +
+          // IMDB rating of the movie
+          "IMDB movie rating (out of 10): " + movieInfo.imdbRating + "\r\n" +
+          // rotten tomatoes rating of the movie
+          "Rotten Tomatoes rating (out of 100%): " + movieInfo.Ratings[1].Value + "\r\n" +
+          // country where the movie was produced.
+          "Filmed in: " + movieInfo.Country + "\r\n" +
+          // language of the movie.
+          "Language: " + movieInfo.Language + "\r\n" +
+          // plot of the movie.
+          "Movie plot: " + movieInfo.Plot + "\r\n" +
+          // actors in the movie.
+          "Actors: " + movieInfo.Actors + "\r\n" +
+          // line break
+          "======================================================================================================="
+
+        console.log(movieResult);
+        // output the movie information to the log.txt file.
+        logData(movieResult);
+      }
+    });
+  });
+
+  function findConcert() {
+    
   }
 }
